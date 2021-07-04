bd33c6a483c8dc7fcab49969c472631e15b42a15
Brian Maloney <3286425+brian-maloney@users.noreply.github.com>
2021-07-04 17:06:27 -0400

    Include dtbs in tar

--- alpine.sh
+++ alpine.sh
@@ -9,6 +9,8 @@ sudo dpkg -x armbian-firmware_21.08.0-trunk_all.deb "$tmpdir"
 sudo mv $tmpdir/boot/config-* $tmpdir/boot/config-lts
 sudo mv $tmpdir/boot/System.map-* $tmpdir/boot/System.map-lts
 sudo mv $tmpdir/boot/vmlinuz-* $tmpdir/boot/vmlinuz-lts
+sudo mkdir $tmpdir/boot/dtbs-lts
+sudo mv $tmpdir/usr/lib/linux-image-current-rockchip64/* $tmpdir/boot/dtbs-lts
 sudo mksquashfs "$tmpdir/lib" "$tmpdir/boot/modloop-lts" -b 1048576 -comp xz -Xdict-size 100%
 sudo tar -C "$tmpdir" -cvf alpine-boot.tar boot
 sudo rm -rf "$tmpdir"
