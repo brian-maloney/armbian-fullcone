c1214276515d068a0b4e69aa644ca5c62239ee87
Brian P. Maloney <3286425+brian-maloney@users.noreply.github.com>
2021-12-11 19:58:02 +0000

    Allow for multiple armbian versions

--- alpine.sh
+++ alpine.sh
@@ -4,8 +4,8 @@ set -e
 
 tmpdir=$(sudo mktemp -d -p .)
 
-sudo dpkg -x linux-image-current-rockchip64_21.08.0-trunk_arm64.deb "$tmpdir"
-sudo dpkg -x armbian-firmware_21.08.0-trunk_all.deb "$tmpdir"
+sudo dpkg -x linux-image-current-rockchip64_*-trunk_arm64.deb "$tmpdir"
+sudo dpkg -x armbian-firmware_*-trunk_all.deb "$tmpdir"
 sudo mv $tmpdir/boot/config-* $tmpdir/boot/config-lts
 sudo mv $tmpdir/boot/System.map-* $tmpdir/boot/System.map-lts
 sudo mv $tmpdir/boot/vmlinuz-* $tmpdir/boot/vmlinuz-lts
